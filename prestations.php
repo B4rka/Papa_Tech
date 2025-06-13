9cf99054c3bcb8f7c0f4c2f30da6aea890a9150e
B4rka <p.e.dejonghe@gmail.com>
2025-06-13 10:39:42 +0200

    Remplissage page prestations

--- prestations.php
+++ prestations.php
@@ -1,5 +1,7 @@
 <?php
     $current_page = basename($_SERVER['PHP_SELF']);
+
+    include __DIR__ . "/services.php";
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +22,80 @@
 
     <main>
         <h1>Nos prestations</h1>
+        <div class="tarifs-container">
+            <h2 class="tarifs-title">Papa Tech - Tarifs 2025</h2>
+            <h3 class="offres">
+                <p>Offre Standard</p>
+                <p>Offre Sénior</p>
+            </h3>
+            <h3>A domicile</h3>
+            <div class="offre-container">
+                <div class="offre-texte">
+                    <p><?= $services['assistance']['nom'] ?></p>
+                    <p><?= $services['assistance']['descriptif'] ?></p>
+                </div>
+                <div class="offre-tarif-standard">
+                    <?= $services['assistance']['offre standard']?>
+                </div>
+                <div class="offre-tarif-sénior">
+                    <?= $services['assistance']['offre sénior']?>
+                </div>
+            </div>
+            <div class="offre-container">
+                <div class="offre-texte">
+                    <p><?= $services['sérénité']['nom'] ?></p>
+                    <p><?= $services['sérénité']['descriptif'] ?></p>
+                </div>
+                <div class="offre-tarif-standard">
+                    <?= $services['sérénité']['offre standard']?>
+                </div>
+                <div class="offre-tarif-sénior">
+                    <?= $services['sérénité']['offre sénior']?>
+                </div>
+            </div>
+            <h3>A distance</h3>
+            <div class="offre-container">
+                <div class="offre-texte">
+                    <p><?= $services['visio']['nom'] ?></p>
+                    <p><?= $services['visio']['descriptif'] ?></p>
+                </div>
+                <div class="offre-tarif-standard">
+                    <?= $services['visio']['offre standard']?>
+                </div>
+                <div class="offre-tarif-sénior">
+                    <?= $services['visio']['offre sénior']?>
+                </div>
+            </div>
+            <div class="offre-container">
+                <div class="offre-texte">
+                    <h3><?= $services['formation express']['nom'] ?></h3>
+                    <p><?= $services['formation express']['descriptif'] ?></p>
+                </div>
+                <div class="offre-tarif-standard">
+                    <?= $services['formation express']['offre standard']?>
+                </div>
+                <div class="offre-tarif-sénior">
+                    <?= $services['formation express']['offre sénior']?>
+                </div>
+            </div>
+            <div class="offre-container">
+                <div class="offre-texte">
+                    <h3><?= $services['personnalisée']['nom'] ?></h3>
+                    <p><?= $services['personnalisée']['descriptif'] ?></p>
+                    <h4><?= $services['personnalisée']['nom offre1'] ?></h4>
+                    <h4><?= $services['personnalisée']['nom offre2'] ?></h4>
+                </div>
+                <div class="offre-tarif-standard" id="tarif-standard-perso">
+                    <p id="upper-tarif"><?= $services['personnalisée']['offre standard1']?></p>
+                    <p><?= $services['personnalisée']['offre standard2']?></p>
+                </div>
+                <div class="offre-tarif-sénior" id="tarif-sénior-perso">
+                    <p id="upper-tarif"><?= $services['personnalisée']['offre sénior1']?></p>
+                    <p><?= $services['personnalisée']['offre sénior2']?></p>
+                </div>
+            </div>
+        </div>
+
     </main>
 
     <?php include __DIR__ . "/Includes/footer.php"; ?>
---
+++ services.php
@@ -0,0 +1,53 @@
+<?php
+
+$services = [
+    'assistance' => [
+        'nom' => 'Assistance Directe (1 heure)',
+        'descriptif' => 'Dépannage, configuration et optimisation d’un appareil en 1
+            heure, avec des conseils personnalisés et avec pédagogie !',
+        'offre standard' => '60€',
+        'offre sénior' => '50€',
+        'domicile' => 'A domicile'
+    ],
+    'sérénité' => [
+        'nom' => 'Forfait sérénité (48 heures)',
+        'descriptif' => 'Une offre de suivi pendant 48h après mon intervention
+            pour résoudre tout nouveau problème lié à l’utilisation de
+            votre appareil, sans stress et à distance !',
+        'offre standard' => '20€',
+        'offre sénior' => 'Inclus',
+        'domicile' => 'A domicile'
+    ],
+    'visio' => [
+        'nom' => 'Appel / Visio-conférence (30 minutes)',
+        'descriptif' => 'Dépannage, configuration et optimisation d’un appareil
+            via une prise en main, pour vous aider rapidement et
+            efficacement, où que vous soyez !',
+        'offre standard' => '40€',
+        'offre sénior' => '35€',
+        'domicile' => 'A distance'
+    ],
+    'formation express' => [
+        'nom' => 'Pack Formation Express (1 Heure 30 min)',
+        'descriptif' => 'Configuration et mise en service de votre appareil,
+            suivies d\'une formation rapide et personnalisée pour
+            vous apprendre à l\'utiliser facilement et en toute
+            confiance !',
+        'offre standard' => '80€',
+        'offre sénior' => '70€',
+        'domicile' => 'A distance'
+    ],
+    'personnalisée' => [
+        'nom' => 'Assistance personnalisée',
+        'descriptif' => 'Une assistance personnalisée pour résoudre les problèmes
+            ou optimiser plusieurs appareils en 3 ou 5 sessions
+            adaptées à vos besoins, à domicile ou à distance.',
+        'nom offre1' => 'Pack de 3 séances (3 X 1 Heure)',
+        'nom offre2' => 'Pack de 5 séances (5 x 1 Heure)',
+        'offre standard1' => '250€',
+        'offre sénior1' => '200€',
+        'offre standard2' => '400€',
+        'offre sénior2' => '350€',
+        'domicile' => 'A distance'
+    ],
+];
